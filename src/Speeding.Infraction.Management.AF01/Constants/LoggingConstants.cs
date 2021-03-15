eca84b07243ef52eb33e52ca3fa0d0b923a8f659
Mandar Dharmadhikari <fullstackmaddy@gmail.com>
2021-03-15 10:33:21 +0530

    Modified the logging constants

--- src/Speeding.Infraction.Management.AF01/Constants/LoggingConstants.cs
+++ src/Speeding.Infraction.Management.AF01/Constants/LoggingConstants.cs
@@ -7,28 +7,32 @@ namespace  Speeding.Infraction.Management.AF01.Constants
         
         public enum ProcessingFunction
         {
-            DetectFaces,
+            ExtractRegistrationNumber,
+            DetectAndBlurFaces,
             CreateSpeedingTicket,
-            NotifyVehicleOwner,
-            HandleFailures
+            NotifyRegisteredOwner,
+            ManageExeceptions
 
         }
 
         public enum EventId
         {
-            Exceptioned = 001,
+            
 
-            DetectFacesStarted = 101,
-            DetectFacesFinished = 102,
+            ExtractRegistrationNumberStarted = 101,
+            ExtractRegistrationNumberFinished = 102,
 
-            CreateSpeedingTicketStarted = 201,
-            CreateSpeedingTicketFinished = 202,
+            DetectAndBlurFacesStarted = 201,
+            DetectAndBlurFacesFinished = 202,
 
-            NotifyVehicleOwnerStarted = 301,
-            NotifyVehicleOwnerFinished = 302,
+            CreateSpeedingTicketStarted = 301,
+            CreateSpeedingTicketFinished = 302,
 
-            HandleFailuresStarted = 401,
-            HandleFailuresFinished = 402
+            NotifyVehicleOwnerStarted = 401,
+            NotifyVehicleOwnerFinished = 402,
+
+            ManageExeceptionsStarted = 401,
+            ManageExeceptionsFinished = 402
 
 
         }
